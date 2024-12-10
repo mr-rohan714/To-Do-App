006d3d9a708f332b3b13c82fe82d1787d38a25ff
mr-rohan714 <mdraunaque14@gmail.com>
2024-12-10 23:10:30 +0530

    uploading files

---
+++ Todo/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'Todo/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'Todo/add_task.html')
+
